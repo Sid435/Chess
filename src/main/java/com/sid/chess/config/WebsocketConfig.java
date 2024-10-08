e089f0ab00420da5c3babee1c6ab9fd2b5da1d66
Siddharth Kumar <99739245+Sid435@users.noreply.github.com>
2024-10-08 21:57:41 +0530

    fixes

--- src/main/java/com/sid/chess/config/WebsocketConfig.java
+++ src/main/java/com/sid/chess/config/WebsocketConfig.java
@@ -29,8 +29,8 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+                .setAllowedOriginPatterns("*");
+//                .withSockJS();
     }
 
     @Override
