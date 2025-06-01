dfbb25335267b34e6572f59594aa550a2358824c
Siddharth Kumar <99739245+Sid435@users.noreply.github.com>
2025-06-01 10:42:20 +0530

    migrated to SockJs

--- src/main/java/com/sid/chess/config/WebsocketConfig.java
+++ src/main/java/com/sid/chess/config/WebsocketConfig.java
@@ -27,7 +27,8 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*");
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
     @Override
